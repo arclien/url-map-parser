90877d91f917b0203bc29d9326dec2f0f113af7b
Jisuk Yoon <thearclien@gmail.com>
2021-04-14 09:13:07 +0900

    global helmet 수정

--- src/components/GlobalHelmet/GlobalHelmet.js
+++ src/components/GlobalHelmet/GlobalHelmet.js
@@ -6,18 +6,6 @@ import useMobileDetect from 'hooks/useMobileDetect';
 const GlobalHelmet = () => {
   const isMobile = useMobileDetect();
 
-  const renderOg = () => (
-    <>
-      <meta property="og:url" content="https://lostark.game.onstove.com/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Lostark 이미지 공유" />
-      <meta property="og:description" content="Lostark 이미지 공유 하자!!!!!" />
-      <meta
-        property="og:image"
-        content="https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/inspection/img_index_v5.jpg?v=timestamp"
-      />
-    </>
-  );
   return (
     <>
       {!isMobile && (
@@ -26,6 +14,17 @@ const GlobalHelmet = () => {
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1"
           />
+          <meta property="og:url" content="https://lostark.game.onstove.com/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Lostark 이미지 공유" />
+          <meta
+            property="og:description"
+            content="Lostark 이미지 공유 하자!!!!!"
+          />
+          <meta
+            property="og:image"
+            content="https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/inspection/img_index_v5.jpg?v=timestamp"
+          />
         </Helmet>
       )}
       {isMobile && (
@@ -33,6 +32,17 @@ const GlobalHelmet = () => {
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
+          <meta property="og:url" content="https://lostark.game.onstove.com/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Lostark 이미지 공유" />
+          <meta
+            property="og:description"
+            content="Lostark 이미지 공유 하자!!!!!"
+          />
+          <meta
+            property="og:image"
+            content="https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/inspection/img_index_v5.jpg?v=timestamp"
           />
         </Helmet>
       )}
